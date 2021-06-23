62a9e969e0963708ce41899e6ad88d4050d3a22e
gsandovalrodri <v-gsandovalrodri@expediagroup.com>
2021-06-22 21:29:44 -0500

    forgot to restore staged changes

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SixFold-Challenge
+
+## Resources
+
+- <https://openflights.org/data.html#route>
+- <https://www.kaggle.com/open-flights/flight-route-database>
+- <https://aviation-edge.com/nearby-airport-and-city-api/>
+
+## TODO
+
+1. Verify that IATA/ICAO are valid inputs (using an API or a dataset holding that information)
+2. ad
---
+++ src/typings/index.d.ts
@@ -0,0 +1,43 @@
+interface IAirport {
+  airport_id: string,
+  name: string,
+  city: string,
+  country: string,
+  iata: string;
+  icao: string;
+  latitude: string;
+  longitude: string;
+  altitude: string;
+  timezone: string;
+  dst: string;
+  tz_database_time_zone: string;
+  type: string;
+  source: string;
+};
+interface IRoute {
+  airline: string;
+  airline_id: string;
+  source_airport: string;
+  source_airport_id: string;
+  destination_apirport: string;
+  destination_airport_id: string;
+  codeshare: string;
+  stops: string;
+  equipment: string;
+}
+
+interface IRouteMap {
+  [key: string]: string[];
+}
+
+interface IAirportMap {
+  [key: string]: {
+    iata: string;
+    icao: string;
+    name: string;
+    city: string;
+    long: string;
+    lat: string;
+    country: string;
+  }
+}
